ac44781e95a77e13fea3c1178e171b12b87f565a
kjanko <becutandavid@gmail.com>
2022-08-18 15:12:54 +0200

    run slurm

--- run_slurm.sh
+++ run_slurm.sh
@@ -14,10 +14,12 @@ git clone https://github.com/gdewael/cpg-transformer.git
 cd cpg-transformer
 conda env create -f environment.yml
 conda activate cpgtransformer
+
 mkdir reproduce_Hemato
 cd reproduce_hemato
 wget https://transfer.sh/41TJ0m/pos.npz
 wget https://transfer.sh/4qR67i/y.npz
 wget https://transfer.sh/GGpjX2/X.npz
+cd ../
 
 python train_cpg_transformer.py reproduce_Hemato/X_ser.npz reproduce_Hemato/y_ser.npz reproduce_Hemato/pos_ser.npz --log_folder logfolder --experiment_name reproduce_results 

153f44645677df7f83681cc77057242d2225f35b
kjanko <becutandavid@gmail.com>
2022-08-24 17:11:37 +0200

    updated run_slurm to use requirements.txt

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-#SBATCH --ntasks-per-node=2
-#SBATCH --time=1:00:00
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1-00:00:00
 #SBATCH --job-name=test_job
-#SBATCH --mem=1G
-#SBATCH --error=testerror_%j.error
+#SBATCH --mem=16G
+#SBATCH --error=%j.error
 #SBATCH --cpus-per-task=1
-#SBATCH --output=testoutput_%j.out
-#SBATCH --gres=gpu:2
+#SBATCH --output=%j.out
 #SBATCH --nodelist=cuda1
+#SBATCH --partition=gpu_queue
+
+echo
+echo Setting up virtual environment...
+echo
+
 export PATH="/opt/anaconda3/bin:$PATH"
 source /opt/anaconda3/etc/profile.d/conda.sh
-git clone https://github.com/gdewael/cpg-transformer.git
-cd cpg-transformer
-conda env create -f environment.yml
-conda activate cpgtransformer
 
-mkdir reproduce_Hemato
-cd reproduce_hemato
-wget https://transfer.sh/41TJ0m/pos.npz
-wget https://transfer.sh/4qR67i/y.npz
-wget https://transfer.sh/GGpjX2/X.npz
-cd ../
+conda config --set ssl_verify no
+
+echo environments list
+conda env list
+echo environments list end
+
+conda create -n cpgtransformer python=3.9
+conda activate cpgtransformer
+pip install -r requirements.txt
+
+echo
+echo Environment activated...
+echo
 
 python train_cpg_transformer.py reproduce_Hemato/X_ser.npz reproduce_Hemato/y_ser.npz reproduce_Hemato/pos_ser.npz --log_folder logfolder --experiment_name reproduce_results 
